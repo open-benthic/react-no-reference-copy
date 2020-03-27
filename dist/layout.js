795df3ef4cf3b556417cb0a4603b76bb62321c12
unknown <omicalpha3@gmail.com>
2020-03-27 04:35:14 +0430

    react library added

--- dist/layout.js
+++ dist/layout.js
@@ -4,6 +4,11 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 const headerStyles = {
   color: '#a7a7a7'
 };
@@ -11,15 +16,15 @@ const styles = {
   backgroundColor: '#00BFFF'
 };
 
-const ViewLayer = props => /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h2", {
+const ViewLayer = props => /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement("h2", {
   style: headerStyles
-}, "Hello"), /*#__PURE__*/React.createElement("h2", {
+}, "Hello"), /*#__PURE__*/_react.default.createElement("h2", {
   style: headerStyles
 }, props.name, " !"));
 
-const ParentLayer = () => /*#__PURE__*/React.createElement("div", {
+const ParentLayer = () => /*#__PURE__*/_react.default.createElement("div", {
   style: styles
-}, /*#__PURE__*/React.createElement(ViewLayer, null));
+}, /*#__PURE__*/_react.default.createElement(ViewLayer, null));
 
 var _default = ParentLayer;
 exports.default = _default;
--- src/layout.jsx
+++ src/layout.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const headerStyles = {
   color: '#a7a7a7'
 };
